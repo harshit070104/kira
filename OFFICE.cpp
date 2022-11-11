aeb426e7ac0e14b69d76ca45e0970c237e9c247f
harshit070104 <105371858+harshit070104@users.noreply.github.com>
2022-11-11 19:23:27 +0530

    Add files via upload
    upload office file

---
+++ OFFICE.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int t,x,y;
+    cin>>t;
+    for(int i=1 ; i<=t ; i++)
+    {
+        cin>>x>>y;
+        cout<<(4*x) + y<<endl;
+    }
+
+    return 0;
+}
